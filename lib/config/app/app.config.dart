ea186976c3a715561d93fd5ccb5ac5e173078dae
Timothy Juma <tjselevani@gmail.com>
2024-09-23 11:49:17 +0300

    workflow update

--- lib/config/app/app.config.dart
+++ lib/config/app/app.config.dart
@@ -8,10 +8,13 @@ class AppConfig {
       'https://i.fbcd.co/products/resized/resized-750-500/d4c961732ba6ec52c0bbde63c9cb9e5dd6593826ee788080599f68920224e27d.jpg';
 
   static const stripeSecretKey = '';
+
   static const stripePublishableKey =
       'pk_test_51PeCk82KeltPeRS6s7XwfsJNm6jrFC3UmWWMVmGkSy33E8dqxYuRgrx2wVx0ViqQLys6D96mDB0J7rlzi66E6Scj00Y0dD3Vnh';
+
   static const paypalClientID =
       'AXQv7O2W0WOVUU2Z6R3QnXWMV5dj0tZhtf9gUIx4RlpEbM_OyCDLe9psBmjpE1sTc7mq8cxWmi5syeiE';
+      
   static const paypalSecretKey =
       'EFm7UCatPL83olEEWP816vnJYWRmQag4j_Hhu0kGzryHoI2sJOIPmTzrjH3vz_BnuqirO_ypO3awovbK';
 }
