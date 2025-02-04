00772c4792f327723d3ed3e897460245ccf88400
tjselevani <113022835+TJselevani@users.noreply.github.com>
2025-02-04 16:24:09 +0300

    Update app_secrets.dart

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,20 +1,15 @@
 class AppSecrets {
-  static const baseUrl =
-      "https://pix2life-app-server.onrender.com/api/v3"; //https://pix2life-app-server.onrender.com
-  static const baseUrl2 =
-      'https://capital-obviously-terrier.ngrok-free.app/api/v3';
+  static const baseUrl = "https://pix2life-app-server.onrender.com/api/v3"; 
+  
+  static const baseUrl2 = '';
 
-  static const kTestUrl =
-      "https://66ec4b962b6cf2b89c5dfe2a.mockapi.io/api/users";
+  static const kTestUrl = "";
 
   static const stripeSecretKey = '';
 
-  static const stripePublishableKey =
-      'pk_test_51PeCk82KeltPeRS6s7XwfsJNm6jrFC3UmWWMVmGkSy33E8dqxYuRgrx2wVx0ViqQLys6D96mDB0J7rlzi66E6Scj00Y0dD3Vnh';
+  static const stripePublishableKey = '';
 
-  static const paypalClientID =
-      'AXQv7O2W0WOVUU2Z6R3QnXWMV5dj0tZhtf9gUIx4RlpEbM_OyCDLe9psBmjpE1sTc7mq8cxWmi5syeiE';
+  static const paypalClientID = '';
 
-  static const paypalSecretKey =
-      'EFm7UCatPL83olEEWP816vnJYWRmQag4j_Hhu0kGzryHoI2sJOIPmTzrjH3vz_BnuqirO_ypO3awovbK';
+  static const paypalSecretKey = '';
 }
