ded7aa69e79728feeb13d23904baa239e5fb04f2
tjselevani <113022835+TJselevani@users.noreply.github.com>
2025-02-04 16:22:40 +0300

    Delete android directory

--- android/app/src/main/kotlin/com/example/pix2life/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pix2life
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity()
